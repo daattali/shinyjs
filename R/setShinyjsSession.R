468c6aa09242b889da6f772fa3bfe8d5a6497e32
Dean Attali <daattali@gmail.com>
2015-03-04 14:57:19 -0800

    check that session object is a real session

--- R/setShinyjsSession.R
+++ R/setShinyjsSession.R
@@ -1,5 +1,8 @@
 #' @export
 setShinyjsSession <- function(session) {
+  if (!inherits(session, "ShinySession")) {
+    stop("'session' is not a ShinySession object.", call. = FALSE)
+  }
   session$onSessionEnded(closeSession)
   assign(".session", session, shinyjsGlobals)
 }
