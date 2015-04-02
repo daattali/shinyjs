09b1b3427473040c346877173aa6bab34aa31a02
Dean Attali <daattali@gmail.com>
2015-04-01 17:12:01 -0700

    don't fail if using CRAN version of shiny, just make a looser check

--- R/utils.R
+++ R/utils.R
@@ -7,5 +7,11 @@ closeSession <- function() {
 }
 
 isSession <- function(x) {
-  inherits(x, "ShinySession")
+  # the dev version of Shiny which isn't on CRAN has a better check for shiny
+  # objects since I filed a bug report for it
+  if (compareVersion(as.character(packageVersion("shiny")), "0.11.1.9003") == -1) {
+    is.environment(x)
+  } else {
+    inherits(x, "ShinySession")
+  }
 }
