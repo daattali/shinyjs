af3a25c7b814db932e1da9e4652292ecffa389cf
Dean Attali <daattali@gmail.com>
2015-05-30 02:34:41 -0700

    allow `hidden` to work for either mutliple tags or a tagList or list

--- R/hidden.R
+++ R/hidden.R
@@ -3,7 +3,7 @@
 #' Create a Shiny tag that is invisible when the Shiny app starts. The tag can
 #' be made visible later with \code{shinyjs::toggle} or \code{shinyjs::show}.
 #'
-#' @param tag Shiny tag to make invisible
+#' @param ... Shiny tag (or tagList or list of of tags) to make invisible
 #' @seealso \code{\link[shinyjs]{useShinyjs}},
 #' \code{\link[shinyjs]{toggle}},
 #' \code{\link[shinyjs]{show}},
@@ -28,14 +28,27 @@
 #'     }
 #'   )
 #' }
+#'
+#' hidden(span(id = "a"), div(id = "b"))
+#' hidden(shiny::tagList(span(id = "a"), div(id = "b")))
+#' hidden(list(span(id = "a"), div(id = "b")))
 #' @export
-hidden <- function(tag) {
-  if (!inherits(tag, "shiny.tag")) {
-    errMsg("'tag' is not a valid Shiny tag")
-  }
+hidden <- function(...) {
+  tags <- list(...)
 
-  tag <- shiny::tagAppendAttributes(
-    tag,
-    class = "shinyjs-hide shinyjs-hidden-init")
-  tag
+  # recursively add the hidden class to all tags
+  if (length(tags) == 1 && inherits(tags[[1]], "shiny.tag")) {
+    tags[[1]] <-
+      shiny::tagAppendAttributes(
+        tags[[1]],
+        class = "shinyjs-hide shinyjs-hidden-init"
+      )
+    return( tags[[1]] )
+  } else if (length(tags) == 1 && is.list(tags[[1]])) {
+    return( lapply(tags[[1]], hidden) )
+  } else if (length(tags) > 1) {
+    return( lapply(tags, hidden) )
+  } else {
+    errMsg("Invalid shiny tags given to `hidden`")
+  }
 }
--- tests/testthat/test-hidden.R
+++ tests/testthat/test-hidden.R
@@ -1,9 +1,13 @@
 context("hidden")
 
+getClasses <- function(tag) {
+  unlist(strsplit(tag$attribs$class, " "))
+}
+
 clsName <- "shinyjs-hide"
 
 test_that("hidden fails on plain text", {
-  expect_error(hidden("abc"), "not a valid Shiny tag")
+  expect_error(hidden("abc"), "Invalid shiny tag")
 })
 
 test_that("hidden works on simple div", {
@@ -29,4 +33,31 @@ test_that("hidden works when div already contains a class", {
   # I sent a pull request to htmltools to fix this, still waiting for them
   # to incorporate my code
   #expect_true(clsName %in% hidden(div("abc", class = "test"))$attribs$class)
+})
+
+test_that("hidden errors when one of multiple tags is not a tag", {
+  expect_error(hidden(shiny::p("abc"), "abc"), "Invalid shiny tag")
+  expect_error(hidden(list(shiny::p("abc"), "abc")), "Invalid shiny tag")
+  expect_error(hidden(shiny::tagList(shiny::p("abc"), "abc")), "Invalid shiny tag")
+})
+
+test_that("hidden works when given multiple tags", {
+  res <- hidden(shiny::p("abc"), shiny::span("abc"))
+  expect_equal(length(res), 2)
+  expect_true(clsName %in% getClasses(res[[1]]))
+  expect_true(clsName %in% getClasses(res[[2]]))
+})
+
+test_that("hidden works when given list", {
+  res <- hidden(list(shiny::p("abc"), shiny::span("abc")))
+  expect_equal(length(res), 2)
+  expect_true(clsName %in% getClasses(res[[1]]))
+  expect_true(clsName %in% getClasses(res[[2]]))
+})
+
+test_that("hidden works when given tagList", {
+  res <- hidden(shiny::tagList(shiny::p("abc"), shiny::span("abc")))
+  expect_equal(length(res), 2)
+  expect_true(clsName %in% getClasses(res[[1]]))
+  expect_true(clsName %in% getClasses(res[[2]]))
 })
