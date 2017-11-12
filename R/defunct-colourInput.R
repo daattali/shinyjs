a5af8199e57c6224e08de8ad188045a90e5478cc
Dean Attali <daattali@gmail.com>
2017-11-12 00:28:09 -0500

    make the defunct functions internal to hopefully get rid of check error complaining that they're not documented

--- R/defunct-colourInput.R
+++ R/defunct-colourInput.R
@@ -1,5 +1,6 @@
 #' @rdname shinyjs-defunct
 #' @export
+#' @keywords internal
 colourInput <- function(...) {
   .Defunct("colourInput", package = "colourpicker",
            msg = "colourInput() has been moved to the 'colourpicker' package.")
@@ -7,6 +8,7 @@ colourInput <- function(...) {
 
 #' @rdname shinyjs-defunct
 #' @export
+#' @keywords internal
 updateColourInput <- function(...) {
   .Defunct("updateColourInput", package = "colourpicker",
            msg = "updateColourInput() has been moved to the 'colourpicker' package.")
@@ -14,6 +16,7 @@ updateColourInput <- function(...) {
 
 #' @rdname shinyjs-defunct
 #' @export
+#' @keywords internal
 colourPicker <- function(...) {
   .Defunct("colourPicker", package = "colourpicker",
            msg = "colourPicker() has been moved to the 'colourpicker' package.")
