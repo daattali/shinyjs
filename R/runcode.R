22f3fa2903c5910d4137069ae9d91d7fee26cf3f
Dean Attali <daattali@gmail.com>
2018-07-14 20:38:08 -0400

    deprecate includeShinyjs param from runcodeUI

--- R/runcode.R
+++ R/runcode.R
@@ -26,9 +26,8 @@
 #' \code{type="ace"})
 #' @param height The height of the editable code input (ignored when
 #' \code{type="text"})
-#' @param includeShinyjs Set this to \code{TRUE} only if your app does not have
-#' a call to \code{useShinyjs()}. If you are already calling \code{useShinyjs()}
-#' in your app, do not use this parameter.
+#' @param includeShinyjs Deprecated. You should always make sure to initialize
+#' shinyjs using \code{\link[shinyjs]{useShinyjs}}.
 #' @seealso \code{\link[shinyjs]{useShinyjs}}
 #' @examples
 #' if (interactive()) {
@@ -52,7 +51,12 @@ runcodeUI <- function(code = "",
                       type = c("text", "textarea", "ace"),
                       width = NULL,
                       height = NULL,
-                      includeShinyjs = FALSE) {
+                      includeShinyjs = NULL) {
+  if (!missing(includeShinyjs)) {
+    warning("`includeShinyjs` argument is deprecated. You should always make ",
+            "sure to initialize shinyjs using `useShinyjs()`.")
+  }
+
   type <- match.arg(type)
 
 	if (type == "ace") {
@@ -63,8 +67,6 @@ runcodeUI <- function(code = "",
 
   placeholder <- "Enter R code"
   shiny::singleton(shiny::tagList(
-    if (includeShinyjs)
-      useShinyjs(),
     if (type == "text")
       shiny::textInput(
         "runcode_expr", label = NULL, value = code,
