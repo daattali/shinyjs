aadcb2f76bf3ebe7f76713fb8dd8a248de164418
Jacob Wujciak-Jens <jacob@wujciak.de>
2022-05-05 22:54:50 +0200

    Reset Radiobuttons with selected = character(0) (#220); fixes #85

--- R/reset.R
+++ R/reset.R
@@ -119,6 +119,9 @@ reset <- function(id = "", asis = FALSE) {
         # most input update functions use 'value' argument, some use 'selected',
         # DateRange uses 'start' and 'end'
         if (type == "RadioButtons") {
+          if (is.null(value) && utils::packageVersion("shiny") > "1.5.0") {
+            value <- character(0)
+          }
           funcParams[['selected']] <- value
         } else if (type == "CheckboxGroup" || type == "Select") {
           if (value == '""') {
