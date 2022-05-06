82059b130c5a86114618aae4f9138bb7ade9085c
Dean Attali <daattali@gmail.com>
2022-05-06 17:28:30 -0400

    more disabled() tests

--- tests/testthat/test-shiny-disabled.R
+++ tests/testthat/test-shiny-disabled.R
@@ -3,19 +3,27 @@ testthat::skip_on_cran()
 library(shiny)
 library(shinytest2)
 
-app <- shinyApp(
-  ui = fluidPage(
-    useShinyjs(),
-    disabled(
-      textInput("text1", "text1", "text1"),
-      fileInput("file1", "file1"),
-    )
-  ),
-  server = function(input, output) {}
+ui_shinyjs <- fluidPage(
+  useShinyjs(),
+  disabled(
+    textInput("text1", "text1", "text1"),
+    fileInput("file1", "file1"),
+  )
 )
+ui_no_shinyjs <- fluidPage(
+  disabled(
+    textInput("text1", "text1", "text1"),
+    fileInput("file1", "file1"),
+  )
+)
+get_app <- function(ui) shinyApp(ui, function(input, output) {})
 
 test_that("{shinytest2} disabled elements get disabled class", {
-  app <- AppDriver$new(app, name = "disabled-class")
-  expect_true(app$get_js('$("#text1").hasClass("disabled")'))
-  expect_true(app$get_js('$("#file1").closest(".btn-file").hasClass("disabled")'))
+  app_shinyjs <- AppDriver$new(get_app(ui_shinyjs))
+  expect_true(app_shinyjs$get_js('$("#text1").hasClass("disabled")'))
+  expect_true(app_shinyjs$get_js('$("#file1").closest(".btn-file").hasClass("disabled")'))
+
+  app_no_shinyjs <- AppDriver$new(get_app(ui_no_shinyjs))
+  expect_false(app_no_shinyjs$get_js('$("#text1").hasClass("disabled")'))
+  expect_false(app_no_shinyjs$get_js('$("#file1").closest(".btn-file").hasClass("disabled")'))
 })
