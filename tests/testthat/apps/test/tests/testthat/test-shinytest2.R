6f7ad098f74c1f085a243b3c71013ca149b4021d
Dean Attali <daattali@gmail.com>
2023-01-16 17:20:42 -0500

    dont test shinytest2 apps

--- tests/testthat/apps/test/tests/testthat/test-shinytest2.R
+++ tests/testthat/apps/test/tests/testthat/test-shinytest2.R
@@ -6,11 +6,3 @@ test_that("test1", {
   app$click("go")
   app$expect_values()
 })
-
-test_that("test2", {
-  app <- AppDriver$new(name = "test2", expect_values_screenshot_args = FALSE)
-  app$expect_values()
-  app$click("go")
-  app$click("go")
-  app$expect_values()
-})
