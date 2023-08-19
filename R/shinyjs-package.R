3a95870216dca340133133d7d18694b70a2f0dea
Dean Attali <daattali@gmail.com>
2023-08-19 19:30:11 -0400

    add correct package file docs (see r-lib/roxygen2#1491)

---
+++ R/shinyjs-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+#' Defunct functions in shinyjs
+#'
+#' \itemize{
+#'   \item{\bold{colourInput()}} {Moved to the \code{colourpicker} package.}
+#'   \item{\bold{updateColourInput()}} {Moved to the \code{colourpicker} package.}
+#'   \item{\bold{colourPicker()}} {Moved to the \code{colourpicker} package.}
+#' }
+#'
+#' @name shinyjs-defunct
+NULL
